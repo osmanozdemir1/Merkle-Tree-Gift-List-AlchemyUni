a6831f322452483bf949742944b4876d404499e8
Osman Ozdemir <ozdemirosman2@gmail.com>
2023-01-02 12:21:27 +0000

    first commit

--- server/index.js
+++ server/index.js
@@ -6,16 +6,14 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '088d3c1ec641306d31cd1e27a5f93ff2d3d9dedd612dadbe731c0d24cdbc7a65';
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  // Grab the parameters from the client side
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  // Prove that a name is in the list 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
